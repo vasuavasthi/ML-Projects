2cf797d22159f7550973782ad1ab1531ab3e432c
vasuavasthi <78016919+vasuavasthi@users.noreply.github.com>
2024-01-02 22:39:14 +0530

    Update CPP_WebApp.py

--- Car Price Prediction/CPP_WebApp.py
+++ Car Price Prediction/CPP_WebApp.py
@@ -10,7 +10,7 @@ import pickle
 import streamlit as st
 
 #loading the saved mode
-loaded_model=pickle.load(open('Car Price Prediction/CPP_trained_model.sav','rb')) 
+loaded_model=pickle.load(open('Car Price Prediction/trained_model.sav','rb')) 
 
 #creating function for prediction
 def Car_price_predictor(input_data):
